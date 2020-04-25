e419ec0a46f3ece92d1a54110f7bd57018be66ab
Tiyani Maswanganyi <tiyanimaswanganyi@gmail.com>
2020-04-25 15:01:36 +0200

    Updated the app to have the questions working with an answers tracker

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,22 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+
+  List <String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true
+  ];
+
+  int currentQuestion = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                 questions[currentQuestion],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +78,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  print(currentQuestion);
+                  if(answers[currentQuestion]) {
+                    scorekeeper.add(Icon(Icons.check, color: Colors.green));
+                  }
+                  else {
+                    scorekeeper.add(Icon(Icons.close, color: Colors.red));
+                  }
+
+                   if(currentQuestion < questions.length -1) {
+                     currentQuestion++;
+                   }
+                });
               },
             ),
           ),
@@ -80,11 +109,25 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  print(currentQuestion);
+                  if(!answers[currentQuestion]) {
+                    scorekeeper.add(Icon(Icons.check, color: Colors.green));
+                  }
+                  else {
+                    scorekeeper.add(Icon(Icons.close, color: Colors.red));
+                  }
+
+                  if(currentQuestion < questions.length - 1) {
+                    currentQuestion++;
+                  }
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scorekeeper,)
+
       ],
     );
   }
